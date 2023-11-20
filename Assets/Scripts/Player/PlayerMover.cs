7b30d4fa2addca3e9419499525d57d9693d4beba
Tsaplin Sergey <74894518+talatari@users.noreply.github.com>
2023-11-20 16:02:47 +0300

    Add external character asset + implementation draft PlayerMover.

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -2,5 +2,38 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+   [SerializeField, Range(1, 10)] private float _moveSpeed = 2f;
 
+   private GameObject _selectedGameObject;
+   private Camera _camera;
+   private Vector3 _targetMove;
+
+   private void Awake() => _camera = Camera.main;
+
+   private void Update()
+   {
+      InputPlayer();
+
+      MoveToTarget();
+   }
+
+   private void InputPlayer()
+   {
+      if (Input.GetMouseButtonDown(0))
+      {
+         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+         if (Physics.Raycast(ray, out RaycastHit raycastHit))
+            _targetMove = raycastHit.point;
+      }
+   }
+   
+   private void MoveToTarget()
+   {
+      if (_targetMove != Vector3.zero)
+      {
+         transform.position = Vector3.MoveTowards(transform.position, _targetMove, _moveSpeed * Time.deltaTime);
+         transform.rotation = Quaternion.LookRotation(_targetMove);
+      }
+   }
 }
